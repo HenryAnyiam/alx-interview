8b75e9025db5f10f7f2312ba252865082cd7e55b
HenryAnyiam <anyiamhenry7@gmail.com>
2023-12-05 10:54:58 +0100

    unlock boxes

---
+++ 0x01-lockboxes/0-lockboxe.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Lockboxes interview question"""
+
+
+def canUnlockAll(boxes):
+    """function to unluck boxes"""
+    if not isinstance(boxes, list):
+        return False
+    length = len(boxes)
+    if length <= 1:
+        return True
+    keys = set(boxes[0]) if isinstance(boxes[0], list) else None
+    keys.add(0)
+    unlock = True
+    all_box = [i for i in range(length)]
+    unlocked = keys
+    while unlock:
+        hold = set()
+        for i in keys:
+            try:
+                hold = hold.union(set(boxes[i]))
+            except (IndexError, TypeError) as e:
+                pass
+        if not hold.issubset(unlocked):
+            keys = hold
+            unlocked = unlocked.union(keys)
+        else:
+            unlock = False
+    if unlocked != set(all_box):
+        return False
+    return True
--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,34 +1,27 @@
 #!/usr/bin/python3
-"""Lockboxes interview question"""
+"""
+lockboxes
+"""
 
 
 def canUnlockAll(boxes):
-    """function to unluck boxes"""
-    if not isinstance(boxes, list):
-        return False
-    length = len(boxes)
-    if length <= 1:
+    """
+    Check if all boxes can be opened
+    :param: boxes: list of boxes
+        A key with the same number as a box opens that box
+        You can assume all keys will be positive integers
+        The first box boxes[0] is unlocked
+
+    :return: True if all boxes can be opened, else return False
+    """
+    unlocked = [0]
+
+    for box_id, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            if key < len(boxes) and key not in unlocked and key != box_id:
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
         return True
-    locked = length - 1
-    keys = set(boxes[0]) if isinstance(boxes[0], list) else None
-    if not keys:
-        return False
-    keys.add(0)
-    unlock = True
-    all_box = [i for i in range(length)]
-    unlocked = keys
-    while unlock:
-        hold = set()
-        for i in keys:
-            try:
-                hold = hold.union(set(boxes[i]))
-            except (IndexError, TypeError) as e:
-                pass
-        if not hold.issubset(unlocked):
-            keys = hold
-            unlocked = unlocked.union(keys)
-        else:
-            unlock = False
-    if unlocked != set(all_box):
-        return False
-    return True
+    return False
