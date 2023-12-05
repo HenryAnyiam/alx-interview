8434bd01ec3108f1da078f5091db7d88053de55a
HenryAnyiam <anyiamhenry7@gmail.com>
2023-12-05 11:14:31 +0100

    unlock boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
-"""
-lockboxes
-"""
+"""LockBoxes"""
 
 
 def canUnlockAll(boxes):
+    """Function to check if all boxes can be unlocked
+       Boxes is a list of lists
     """
-    Check if all boxes can be opened
-    :param: boxes: list of boxes
-        A key with the same number as a box opens that box
-        You can assume all keys will be positive integers
-        The first box boxes[0] is unlocked
 
-    :return: True if all boxes can be opened, else return False
-    """
-    unlocked = [0]
-
-    for box_id, box in enumerate(boxes):
-        if not box:
+    unlocked = {0}
+    length = len(boxes)
+    for index, value in enumerate(boxes):
+        if not value:
             continue
-        for key in box:
-            if key < len(boxes) and key not in unlocked and key != box_id:
-                unlocked.append(key)
-    if len(unlocked) == len(boxes):
-        return True
-    return False
+        for key in value:
+            if (key < length) and (key not in unlocked) and (key != index):
+                unlocked.add(key)
+    if len(unlocked) != length:
+        return False
+    return True
