f0eadd5ff5920fca159361cc9cb4323e25e6f562
HenryAnyiam <anyiamhenry7@gmail.com>
2024-02-08 21:33:39 +0100

    island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,7 +6,11 @@ def island_perimeter(grid: list) -> int:
     """get the island perimeter"""
     count = 0
     perimeter = 0
+    if not isinstance(grid, list):
+        return 0
     for i in grid:
+        if not isinstance(i, list):
+            return 0
         temp = i.count(1)
         if temp != 0:
             perimeter += abs(temp - count)
