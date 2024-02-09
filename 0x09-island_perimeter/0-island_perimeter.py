9833c56a90aa509f90b76cef0b806e66dcd944de
HenryAnyiam <anyiamhenry7@gmail.com>
2024-02-09 10:48:38 +0100

    fix

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,13 +8,19 @@ def island_perimeter(grid: list) -> int:
     perimeter = 0
     if not isinstance(grid, list):
         return 0
+    l_index = None
+    c_index = None
     for i in grid:
         if not isinstance(i, list):
             return 0
         temp = i.count(1)
         if temp != 0:
+            c_index = i.index(1)
+            if temp == count and l_index is not None and c_index != l_index:
+                perimeter += abs(c_index - l_index) * 2
             perimeter += abs(temp - count)
             perimeter += 2
             count = temp
+            l_index = c_index
     perimeter += count
     return perimeter
